3d5f6ec6663cb1f10be1388461dbe26c2a0bc0d7
Patrick Zhang <pqz317@uw.edu>
2023-07-05 11:32:12 +0000

    making utils work for normal filesystems

--- spike_tools/general.py
+++ spike_tools/general.py
@@ -4,6 +4,8 @@ import h5py
 import s3fs
 import os
 import time 
+import glob
+import scipy.io as scipy_io
 
 ## S3 Paths
 DATAJOINT_BUCKET = "/l2l.datajoint.data/" 
@@ -20,7 +22,11 @@ def get_subject_session_string(subject, session):
     return "sub-" + subject + "_sess-" + str(session)
 
 def get_subject_sessions(fs, subject, species_dir=NHP_WCST_DIR):
-    all_sessions =  fs.ls(os.path.join(species_dir, "rawdata", "sub-" + str(subject)))
+    dir_path = os.path.join(species_dir, "rawdata", "sub-" + str(subject))
+    if fs is None: 
+        all_sessions = os.listdir(dir_path)
+    else: 
+        all_sessions = fs.ls(dir_path)
     all_sessions = [x.split("sess-")[1] for x in all_sessions if len(x.split("sess-")) > 1]
     all_sessions = [x for x in all_sessions if x != "testsession"]
     return all_sessions 
@@ -41,7 +47,13 @@ def get_channels_path(subject, session, species_dir=NHP_WCST_DIR):
 
 def get_channels_list(fs, subject, session, region="all"):
     channel_file = get_channels_path(subject, session)
-    channel_data = pd.read_csv(fs.open(channel_file))
+    if fs is None: 
+        f = open(channel_file, 'rb')
+    else: 
+        f = fs.open(channel_file)
+    # channel_data = pd.read_csv(fs.open(channel_file))
+    channel_data = pd.read_csv(f)
+
     all_channels = channel_data['1'].to_numpy()
     if region.lower() == "all":
         return all_channels
@@ -52,32 +64,46 @@ def get_channels_list(fs, subject, session, region="all"):
         pfc_channels = np.array([x for x in all_channels if x.find('a') >= 0])
         return pfc_channels
     
-def list_session_units(fs, subject, session):
-    spike_path = get_spike_path(subject, session)
-    unit_files = fs.glob(spike_path + "*spiketimes.mat")
+def list_session_units(fs, subject, session, species_dir=NHP_WCST_DIR):
+    spike_path = get_spike_path(subject, session, species_dir)
+    if fs is None: 
+        unit_files = glob.glob(spike_path + "*spiketimes.mat")
+    else: 
+        unit_files = fs.glob(spike_path + "*spiketimes.mat")
+
     channels = [x.split("chan-")[-1].split("_")[0] for x in unit_files]
     units = [int(x.split("unit-")[-1].split("_")[0]) for x in unit_files]
     unit_ids = list(range(0, len(units)))
     unit_info = pd.DataFrame(np.array([unit_ids, channels, units, unit_files]).T, columns=["UnitID", "Channel", "Unit", "SpikeTimesFile"])
     return unit_info
 
-def get_spike_times(fs, subject, session, channels=[], units=[]):
-    all_unit_info = list_session_units(fs, subject, session)
-    
+def _get_spike_timestamps(fs, x):
+    if fs is None:
+        f = open(x.SpikeTimesFile, 'rb')
+    else: 
+        f = fs.open(x.SpikeTimesFile)
+    return np.squeeze(h5py.File(f).get('timestamps').astype(int))
+
+def get_spike_times(fs, subject, session, channels=[], units=[], species_dir=NHP_WCST_DIR):
+    all_unit_info = list_session_units(fs, subject, session, species_dir)
     if len(channels) == 0:
         filter_rows = all_unit_info
     elif len(channels) > 0 and len(units) == 0:
         filter_rows = all_unit_info[all_unit_info.Channel.isin(channels)]
     elif len(channels) > 0 and len(units) > 0:
         filter_rows = all_unit_info[(all_unit_info.Channel.isin(channels)) & (all_unit_info.Unit.isin(units))]
-    spike_times = filter_rows.apply(lambda x: np.squeeze(h5py.File(fs.open(x.SpikeTimesFile)).get('timestamps')).astype(int), axis=1).explode()
+    spike_times = filter_rows.apply(lambda x: _get_spike_timestamps(fs, x), axis=1).explode()
     spike_times_df = pd.DataFrame(spike_times, index=spike_times.index.values, columns=["SpikeTime"])
     spike_times_df["UnitID"] = spike_times_df.index.values
     return spike_times_df
 
 def get_spike_times_by_trial(fs, subject, session, trials=[], channels=[], units=[], start_field="TrialStart", end_field="TrialEnd", pre_start=0, post_end=0):
     trial_file = get_behavior_path(subject, session)
-    trial_data = pd.read_csv(fs.open(trial_file)) 
+    if fs is None:
+        f = open(trial_file, 'rb')
+    else: 
+        f = fs.open(trial_file)    
+    trial_data = pd.read_csv(f) 
     if len(trials) > 0:   
         trial_data = trial_data[trial_data.TrialNumber.isin(trials)] 
     trial_data.set_index("TrialNumber")
